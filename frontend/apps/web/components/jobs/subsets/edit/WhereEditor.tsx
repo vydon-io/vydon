061ad77044f5d9b0641881c64d5ea04914486ef5
Pierre <20837534+pr-rnc@users.noreply.github.com>
2026-05-17 17:42:03 +0200

    style(frontend): apply prettier to whereeditor

--- frontend/apps/web/components/jobs/subsets/edit/WhereEditor.tsx
+++ frontend/apps/web/components/jobs/subsets/edit/WhereEditor.tsx
@@ -62,7 +62,10 @@ function useAutocomplete(columns: string[]): void {
     const provider = monaco.languages.registerCompletionItemProvider('sql', {
       triggerCharacters: [' ', '.'], // Trigger autocomplete on space and dot
 
-      provideCompletionItems(model: editor.ITextModel, position: { lineNumber: number; column: number }) {
+      provideCompletionItems(
+        model: editor.ITextModel,
+        position: { lineNumber: number; column: number }
+      ) {
         const textUntilPosition = model.getValueInRange({
           startLineNumber: 1,
           startColumn: 1,
